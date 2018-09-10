b4a53e68a537dcc19551deb71409177f149b697e
Deokjin Lee <truevirtue@nexell.co.kr>
2018-09-10 15:47:03 +0900

    modify the nexell license
    This patch fixes the company name typo
    in the license.
    
    Change-Id: I81b118bce9d90bf0429c0bba107c3578c03be5f1
    Signed-off-by: Deokjin Lee <truevirtue@nexell.co.kr>

--- src/lib/libnx.h
+++ src/lib/libnx.h
@@ -7,7 +7,7 @@
  * but not limited to the implied warranties of merchantabulity and/or
  * fitness for a aparticular purpose.
  *
- * This program is copyrighted by Nexcel and does not allow modification or
+ * This program is copyrighted by Nexell and does not allow modification or
  * distribution. In addition to the author (person in charge), the modifier
  * is responsible for the modification.
  */
