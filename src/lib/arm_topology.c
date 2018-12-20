b2aaad764fa50e4e8a5650fa53753d0f0a5ecd58
DeokJin Lee <truevirtue@nexell.co.kr>
2018-12-20 15:38:54 +0900

    changes the Nexell license
    This patch changed the Nexell license to 3-clause
    license.
    
    Change-Id: Ifd8fb7fddb3f6b3a34b6f46db5d9294fc8f65280
    Signed-off-by: DeokJin Lee <truevirtue@nexell.co.kr>

--- src/lib/arm_topology.c
+++ src/lib/arm_topology.c
@@ -1,15 +1,26 @@
-/*
- * Copyright (C) 2018  Nexell Co., Ltd.
- * Author: deoks <truevirtue@nexell.co.kr>
+/* Copyright (C) 2018  Nexell Co., Ltd.
  *
- * Nexell informs that this code and information is provided "as Is" base
- * and without warranty of any kind, either expressed or implied, including,
- * but not limited to the implied warranties of merchantabulity and/or
- * fitness for a aparticular purpose.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the Nexell nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * This program is copyrighted by Nexell and does not allow modification or
- * distribution. In addition to the author (person in charge), the modifier
- * is responsible for the modification.
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL,SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include <arch.h>
 
